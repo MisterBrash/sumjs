422f9a643818bc73293e39a490378f8bee749132
MisterBrash <matthew.brash@ocsb.ca>
2020-09-29 18:08:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+/* This is the global space. Anything you put here gets executed when
+    the JavaScript file is loaded, even if you didn't want it to.
+*/
+
+// This is a function. It only runs when it gets asked ("called") to run 
+function main() {
+  // Write your code inside this function.
+
+
+
+
+}
